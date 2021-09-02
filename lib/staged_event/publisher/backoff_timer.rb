417a3a6abf8e1b75b21454502c4a6fa952d11dbf
Andrew Cross <albany551@gmail.com>
2021-09-02 09:12:29 -0400

    Rename a confusing variable

--- lib/staged_event/publisher/backoff_timer.rb
+++ lib/staged_event/publisher/backoff_timer.rb
@@ -11,23 +11,23 @@ module StagedEvent
       end
 
       def increment
-        @backoff_count += 1
+        @increments += 1
       end
 
       def reset
-        @backoff_count = 0
+        @increments = 0
       end
 
       def value
         # returns 1 until the timer has been incremented n times, after which it
         # returns n^2 (until reaching a set maximum)
-        backoff_time = [ 1, backoff_count - INCREMENTS_BEFORE_BACKOFF + 1 ].max**2
+        backoff_time = [ 1, increments - INCREMENTS_BEFORE_BACKOFF + 1 ].max**2
         [ backoff_time, MAX_VALUE ].min
       end
 
       private
 
-      attr_accessor :backoff_count
+      attr_accessor :increments
     end
   end
 end
