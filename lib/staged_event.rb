7c1ee1021600500112b5bd33b0987db383f804db
Andrew Cross <andrew.cross@freshly.com>
2021-09-30 11:57:00 -0400

    TACO-640 Include the UUID in the serialized message (#10)
    * TACO-640 Rename MessageEnvelope to EventEnvelope to be less confusing
    
    * TACO-640 Update .to_proto to reflect EventEnvelope name change
    
    * TACO-640 The data column shouldn't ever be null
    
    * TACO-640 Precompute the uuid so it can be included in the event data
    
    * TACO-640 Add deserialization method for event data

--- lib/staged_event.rb
+++ lib/staged_event.rb
@@ -2,7 +2,7 @@
 
 require_relative "staged_event/backoff_timer"
 require_relative "staged_event/configuration"
-require_relative "staged_event/message_envelope_pb"
+require_relative "staged_event/event_envelope_pb"
 require_relative "staged_event/model"
 require_relative "staged_event/publisher/base"
 require_relative "staged_event/publisher/google_pub_sub"
@@ -13,19 +13,54 @@ require_relative "staged_event/version"
 require_relative "staged_event/railtie" if defined?(Rails)
 
 module StagedEvent
+  class Error < StandardError; end
+
+  class DeserializationError < Error; end
+  class UnknownEventTypeError < Error; end
+
   class << self
+    # Builds an ActiveRecord model from a proto object representing an event.
+    # If the model is committed to the database, it will be published by the
+    # publisher process.
+    #
+    # @param [Instance of a protobuf generated class] proto the event data to publish
+    # @option kwargs [String] :topic the topic that will receive the event
+    # @return [StagedEvent::Model] an ActiveRecord model ready to be saved
     def from_proto(proto, **kwargs)
-      envelope = MessageEnvelope.new(
-        message: {
+      uuid = SecureRandom.uuid
+
+      envelope = EventEnvelope.new(
+        event: {
           type_url: proto.class.descriptor.name,
           value: proto.class.encode(proto),
         },
+        uuid: uuid,
       )
 
-      data = MessageEnvelope.encode(envelope)
+      data = EventEnvelope.encode(envelope)
       topic = kwargs.fetch(:topic, nil)
 
-      Model.new(data: data, topic: topic)
+      Model.new(id: uuid, data: data, topic: topic)
+    end
+
+    # Converts serialized event data received from a publisher into an object with
+    # the event (and its metadata) in accessible form
+    #
+    # @param [String] serialized_data the serialized event data
+    # @return [OpenStruct] an object representing the event
+    def deserialize_event(serialized_data)
+      envelope = EventEnvelope.decode(serialized_data)
+      event_descriptor = Google::Protobuf::DescriptorPool.generated_pool.lookup(envelope.event.type_url)
+      raise UnknownEventTypeError if event_descriptor.blank?
+
+      proto = event_descriptor.msgclass.decode(envelope.event.value)
+
+      OpenStruct.new(
+        id: envelope.uuid,
+        data: proto,
+      )
+    rescue Google::Protobuf::ParseError
+      raise DeserializationError
     end
   end
 end
