abd034adfb23569f25634cdd5910c9eb43036eb0
Andrew Cross <albany551@gmail.com>
2021-10-07 10:50:22 -0400

    TACO-622 Add configuration for subscriber

--- lib/staged_event/configuration.rb
+++ lib/staged_event/configuration.rb
@@ -11,10 +11,15 @@ module StagedEvent
         option :project_id
         option :credentials
         option :topic_map
+        option :subscription_ids
       end
 
       nested :publisher do
         option :batch_size
+      end
+
+      nested :subscriber do
+        option :event_received_callback
       end
     end
   end
